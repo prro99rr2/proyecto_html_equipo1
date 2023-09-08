c1cef556004a0bee5a5d7f56a9ba54c2d76d900d
prro99rr2 <116322826+prro99rr2@users.noreply.github.com>
2023-09-08 09:57:40 -0300

    Add files via upload

---
+++ web/index2.js
@@ -0,0 +1,11 @@
+
+
+document.getElementById(precio);
+
+document.getElementById(porcentaje).value;
+
+function calcularPorcentaje(){
+    let precio1 = document.getElementById(precio);
+}
+
+console.log (precio*porcentaje/100);
